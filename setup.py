998944f3cf0f4a83479667da14a94cec15896268
pysyun <48061047+pysyun@users.noreply.github.com>
2019-07-05 19:05:13 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup (
+   name = 'jupyter_session',
+   version = '1.0',
+   description = 'Syun\'s Jupyter Notebook extension for handling sessions.',
+   author = 'Py Syun',
+   author_email = 'pysyun@vitche.com',
+   py_modules = ['jupyter.session'],
+   install_requires = []
+)

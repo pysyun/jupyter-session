af9117390ac73c782be6369c1c11707bdcd1abe8
Cho Nakyun <chonakyun@gmail.com>
2019-07-17 08:45:35 +0300

    adding script

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup
 setup (
    name = 'jupyter_session',
    version = '1.0',
+   scripts=['script/jupyter-session-folder.sh'],
    description = 'Syun\'s Jupyter Notebook extension for handling sessions.',
    author = 'Py Syun',
    author_email = 'pysyun@vitche.com',
    py_modules = ['jupyter.session'],
-   install_requires = []
+   install_requires = [],
+   url='https://github.com/pysyun/jupyter-session'
 )
